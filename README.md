978f3daadcb68914841d4582f91a4bb90bc7295d
David McDonald <idavidmcdonald@me.com>
2015-12-23 12:14:27 +0000

    Init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+symfonyproject
+==============
+
+A Symfony project created on October 30, 2015, 11:21 pm.
---
+++ src/Blogger/BlogBundle/Controller/BlogController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * Show a blog entry
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $blog = $em->getRepository('BloggerBlogBundle:Blog')->find($id);
+
+        if (!$blog) {
+            throw $this->createNotFoundException('Unable to find Blog post.');
+        }
+
+        $comments = $em->getRepository('BloggerBlogBundle:Comment')
+                       ->getCommentsForBlog($blog->getId());
+
+        return $this->render('BloggerBlogBundle:Blog:show.html.twig', array(
+            'blog'     => $blog,
+            'comments' => $comments
+        ));
+    }
+}
